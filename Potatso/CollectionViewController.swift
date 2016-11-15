113d6d0386619bdb26f57f56564b34cc3f673fb4
ruqli <ruqli@microsoft.com>
2016-11-15 15:19:25 +0800

    Fix add proxy

--- Potatso/CollectionViewController.swift
+++ Potatso/CollectionViewController.swift
@@ -41,8 +41,26 @@ class CollectionViewController: SegmentPageVC {
             let vc = RuleSetConfigurationViewController(ruleSet: nil)
             navigationController?.pushViewController(vc, animated: true)
         case 1:
-            let vc = ProxyConfigurationViewController(upstreamProxy: nil)
-            navigationController?.pushViewController(vc, animated: true)
+            let alert = UIAlertController(title: "Add Proxy".localized(), message: nil, preferredStyle: .ActionSheet)
+            alert.addAction(UIAlertAction(title: "Import From QRCode".localized(), style: .Default, handler: { (action) in
+                let importer = Importer(vc: self)
+                importer.importConfigFromQRCode()
+            }))
+            alert.addAction(UIAlertAction(title: "Manual Settings".localized(), style: .Default, handler: { (action) in
+                let vc = ProxyConfigurationViewController()
+                self.navigationController?.pushViewController(vc, animated: true)
+            }))
+            alert.addAction(UIAlertAction(title: "CANCEL".localized(), style: .Cancel, handler: nil))
+            if let presenter = alert.popoverPresentationController {
+                if let rightBtn : View = navigationItem.rightBarButtonItem?.valueForKey("view") as? View {
+                    presenter.sourceView = rightBtn
+                    presenter.sourceRect = rightBtn.bounds
+                } else {
+                    presenter.sourceView = segmentedControl
+                    presenter.sourceRect = segmentedControl.bounds
+                }
+            }
+            self.presentViewController(alert, animated: true, completion: nil)
         default:
             break
         }
